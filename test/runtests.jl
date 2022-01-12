932a0772ecdba3c0d562f01f58ab76bdb34e2faa
ST John <st--@users.noreply.github.com>
2022-01-12 12:03:31 +0200

    CI: change 1.3 to 1.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using LinearRegression
 using Test
 
+function check_type_stability(regressor, X)
+    @testset "type-inference" begin
+        @inferred coef(regressor)
+        @inferred regressor(X)
+        @inferred regressor(X[1, :])
+    end
+end
+
 function test_linreg_multivariate(X, y, beta; index=1)
     @testset "weights $(repr(weights))" for weights in (nothing, ones(size(X, 1)))
         @testset "Solver $method" for method in (SolveQR(), SolveCholesky())
@@ -8,18 +16,13 @@ function test_linreg_multivariate(X, y, beta; index=1)
             @test coef(regressor) ≈ beta
             @test regressor(X) ≈ y
             @test regressor(X[index, :]) ≈ y[index]
-
-            @testset "type-inference" begin
-                @inferred coef(regressor)
-                @inferred regressor(X)
-                @inferred regressor(X[index, :])
-            end
+            check_type_stability(regressor, X)
         end
     end
 end
 
 @testset "LinearRegression.jl" begin
-    @testset "scalar" begin
+    @testset "univariate" begin
         X = [4 6]'
         y = [-2, 4]
 
@@ -32,32 +35,29 @@ end
 
                 regressor = linregress(X, y, weights; intercept=false, method=method)
                 @test regressor([0]) == 0
-
-                @testset "type-inference" begin
-                    @inferred coef(regressor)
-                    @inferred regressor([4 5 6]')
-                    @inferred regressor([0])
-                end
+                check_type_stability(regressor, [4 5 6]')
             end
         end
     end
 
-    @testset "intercept $intercept" for intercept in (true, false)
-        X = rand(100, 13)
-        X0 = intercept ? [X ones(size(X, 1))] : X
+    @testset "multivariate" begin
+        @testset "intercept $intercept" for intercept in (true, false)
+            X = rand(100, 13)
+            X0 = intercept ? [X ones(size(X, 1))] : X
 
-        @testset "matrix" begin
-            beta = rand(size(X0, 2))
-            y = X0 * beta
+            @testset "single-output" begin
+                beta = rand(size(X0, 2))
+                y = X0 * beta
 
-            test_linreg(X, y, beta; index=17)
-        end
+                test_linreg(X, y, beta; index=17)
+            end
 
-        @testset "multioutput" begin
-            beta = rand(size(X0, 2), 5)
-            y = X0 * beta
+            @testset "multi-output" begin
+                beta = rand(size(X0, 2), 5)
+                y = X0 * beta
 
-            test_linreg(X, y, beta; index=17)
+                test_linreg(X, y, beta; index=17)
+            end
         end
     end
 end
